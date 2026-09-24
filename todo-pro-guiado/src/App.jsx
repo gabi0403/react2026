f7e10acf3e13ae664407bca63fcf133f4660b53e
gabi0403 <gabi0403m@gmail.com>
2026-09-24 14:13:24 -0300

    24/09

---
+++ todo-pro-guiado/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react"
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+
+const App = () => {
+  //inicializar as informações usando o useState
+  const [tarefas, setTarefas] = useState([]);// vetor inical vazio ( sem Tarefas)
+
+  //criar métodos
+  //addTarefa
+  const addTarefa = (texto) =>{
+    const novaTarefa = {
+      id:crypto.randomUUID(),//gera uma chave aleatória criptografada()
+      texto,
+      completed: false
+    };
+    setTarefas((atualTarefas) => [...atualTarefas, novaTarefa]);//recriar um novo vetor de tarefas com a nova tarefa
+  };
+
+  //modificarTarefa
+  const modificarTarefa = (id) =>{
+    setTarefas(
+      (atualTarefas)=>atualTarefas.map(
+      (tarefa) => tarefa.id === id ? {...tarefa, completed : !tarefa.completed} : tarefa));
+  }
+  
+  //removerTarefa
+  const removerTarefa = (id) => {
+    setTarefas((atualTarefas) => atualTarefas.filter((tarefa)=> tarefa.id !== id));
+  }
+
+  //virtual DOM
+  return(
+    <main>
+      <h1>Lista de Tarefas Todo-Pro</h1>
+      <TodoForm addTarefa={addTarefa}/>
+      <TodoList tarefas={tarefas} modificarTarefa={modificarTarefa} removerTarefa={removerTarefa} />
+    </main>
+  );
+}
+
+export default App;
